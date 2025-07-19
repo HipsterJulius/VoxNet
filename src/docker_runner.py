ef4a8a0080a6022f0ec59a8defa2abb91cea066b
julius.thuenmann <julius.thunemann@uni-oldenburg.de>
2025-07-20 00:15:52 +0200

    läuft durch mit metriken

--- src/docker_runner.py
+++ src/docker_runner.py
@@ -2,28 +2,28 @@
 
 import os
 import time
-import shutil
 from nets.voxNet import VoxNet
 import tensorflow as tf
 import numpy as np
 
 SHARED_DIR = "/shared"
 VOXEL_SRC = os.path.join(SHARED_DIR, "voxel.npy")
-
-PREDICTION_PATH_SRC = "/workspace/VoxNet/src/prediction.txt"
 PREDICTION_PATH_DST = os.path.join(SHARED_DIR, "prediction.txt")
 DONE_PATH = os.path.join(SHARED_DIR, "done.txt")
 
 MODEL_DIR = "/workspace/VoxNet/logs"
 model = VoxNet()
 classifier = tf.estimator.Estimator(model_fn=model.core, model_dir=MODEL_DIR)
+
 print("Modell geladen...")
 print("VoxNet eval runner gestartet...")
 
 while True:
     if os.path.exists(DONE_PATH):
-        print("Stoppsignal empfangen beende Inferenzprozess.")
-        time.sleep(100)
+        print("Stoppsignal empfangen loesche done.txt und fahre fort.")
+        os.remove(DONE_PATH)
+        break 
+
     if os.path.exists(VOXEL_SRC):
         try:
             print("Voxel-Datei entdeckt starte Inferenz...")
@@ -43,14 +43,15 @@ while True:
             pred = next(predictions)
             label_id = int(pred['pred_cls'])
 
-            # Speichern
-            os.makedirs(os.path.dirname(PREDICTION_PATH_DST), exist_ok=True)
+            # Ergebnis speichern
             with open(PREDICTION_PATH_DST, "w") as f:
                 f.write(str(label_id))
-            print("Abgespecihert")
+            print({label_id})
+
+            # Eingabedatei entfernen für nächsten Zyklus
+            os.remove(VOXEL_SRC)
 
         except Exception as e:
             print("Fehler beim Verarbeiten:", e)
     else:
-        print("Kein Dir")
-    time.sleep(0.1)
+        time.sleep(0.1)
