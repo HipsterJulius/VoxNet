70e4d37e9fcdb772a018e90613dc03688f72ed09
julius.thuenmann <julius.thunemann@uni-oldenburg.de>
2025-07-20 22:01:44 +0200

    läuft

--- src/docker_runner.py
+++ src/docker_runner.py
@@ -5,24 +5,23 @@ import time
 from nets.voxNet import VoxNet
 import tensorflow as tf
 import numpy as np
+import absl.logging
 
+absl.logging.set_verbosity(absl.logging.ERROR)
+absl.logging.set_stderrthreshold('error')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 
 SHARED_DIR = "/shared"
 VOXEL_SRC = os.path.join(SHARED_DIR, "voxel.npy")
 PREDICTION_PATH_DST = os.path.join(SHARED_DIR, "prediction.txt")
 DONE_PATH = os.path.join(SHARED_DIR, "done.txt")
-
 MODEL_DIR = "/workspace/VoxNet/logs"
+
 model = VoxNet()
 classifier = tf.estimator.Estimator(model_fn=model.core, model_dir=MODEL_DIR)
-
 print("Modell geladen...")
+
 print("VoxNet eval runner gestartet...")
-import absl.logging
-absl.logging.set_verbosity(absl.logging.ERROR)
-absl.logging.set_stderrthreshold('error')
 while True:
     if os.path.exists(DONE_PATH):
         print("Stoppsignal empfangen.")
@@ -31,8 +30,6 @@ while True:
 
     if os.path.exists(VOXEL_SRC):
         try:
-            #print("Voxel-Datei entdeckt starte Inferenz...")
-
             # Eingabe laden
             voxel = np.load(VOXEL_SRC).astype(np.float32)
             voxel = np.expand_dims(voxel, axis=0)  # (1, 32, 32, 32)
@@ -51,7 +48,6 @@ while True:
             # Ergebnis speichern
             with open(PREDICTION_PATH_DST, "w") as f:
                 f.write(str(label_id))
-            #print({label_id})
 
             # Eingabedatei entfernen für nächsten Zyklus
             os.remove(VOXEL_SRC)
