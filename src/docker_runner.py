d69992a9ee7961d22cdc3ee4462581ae20c51ce8
julius.thuenmann <julius.thunemann@uni-oldenburg.de>
2025-07-20 21:31:50 +0200

    läuft vollständig durch

--- src/docker_runner.py
+++ src/docker_runner.py
@@ -6,6 +6,9 @@ from nets.voxNet import VoxNet
 import tensorflow as tf
 import numpy as np
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
 SHARED_DIR = "/shared"
 VOXEL_SRC = os.path.join(SHARED_DIR, "voxel.npy")
 PREDICTION_PATH_DST = os.path.join(SHARED_DIR, "prediction.txt")
@@ -17,16 +20,18 @@ classifier = tf.estimator.Estimator(model_fn=model.core, model_dir=MODEL_DIR)
 
 print("Modell geladen...")
 print("VoxNet eval runner gestartet...")
-
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
+absl.logging.set_stderrthreshold('error')
 while True:
     if os.path.exists(DONE_PATH):
-        print("Stoppsignal empfangen loesche done.txt und fahre fort.")
+        print("Stoppsignal empfangen.")
         os.remove(DONE_PATH)
         break 
 
     if os.path.exists(VOXEL_SRC):
         try:
-            print("Voxel-Datei entdeckt starte Inferenz...")
+            #print("Voxel-Datei entdeckt starte Inferenz...")
 
             # Eingabe laden
             voxel = np.load(VOXEL_SRC).astype(np.float32)
@@ -46,7 +51,7 @@ while True:
             # Ergebnis speichern
             with open(PREDICTION_PATH_DST, "w") as f:
                 f.write(str(label_id))
-            print({label_id})
+            #print({label_id})
 
             # Eingabedatei entfernen für nächsten Zyklus
             os.remove(VOXEL_SRC)
